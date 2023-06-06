aef57a5be4ea4876e7ac2e51cfa5149f67726216
Sundhar S G <sundhargopal462@outlook.com>
2023-06-06 11:55:13 +0530

    CreatePostComponent Working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CreatepostComponent } from './createpost/createpost.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+const routeConfig: Routes = [
+    {
+        path: "",
+        component: HomeComponent,
+        title: "Influentia - Homepage"
+    },
+    {
+        path: "contents/create-post",
+        component: CreatepostComponent,
+        title: "Influentia - Contents - Create a New Social Media Post"
+    }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatepostComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    RouterModule.forRoot(routeConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
